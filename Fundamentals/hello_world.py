5e0361cb74d2688c93bd9c5689c1d74ba235bb97
cold-snowflake <cold.snowflake01@gmail.com>
2023-05-13 01:43:02 +0300

    codewars challenge

---
+++ Fundamentals/hello_world.py
@@ -0,0 +1,8 @@
+#Make a simple function called greet that returns
+#the most-famous "hello world!".
+#well, how can we do without it
+#the most creative "hello world"
+
+
+def greet():
+    return "hello world!"
