1f387a6c67d05a8807938829d266ea40882aaa98
cold-snowflake <cold.snowflake01@gmail.com>
2023-05-23 22:23:39 +0300

    piracy

---
+++ Fundamentals/object_oriented_piracy.py
@@ -0,0 +1,23 @@
+"""Ahoy matey!
+
+You are a leader of a small pirate crew. And you have a plan.
+With the help of OOP you wish to make a pretty efficient
+system to identify ships with heavy booty on board!
+
+Unfortunately for you, people weigh a lot these days, so
+how do you know if a ship is full of gold and not people?"""
+
+class Ship:
+    def __init__(self, draft, crew):
+        self.draft = draft
+        self.crew = crew
+
+    def is_worth_it(self):
+        if self.draft - self.crew * 1.5 >= 20:
+            return True
+        else:
+            return False
+
+
+Titanic = Ship(105, 10)
+print (Titanic.is_worth_it())
